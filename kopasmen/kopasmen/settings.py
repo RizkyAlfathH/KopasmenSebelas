2b69197ecdb334af0bb4a96a218ea07373b6dfc9
fath <rizkyalfath80@gmail.com>
2025-10-30 12:10:08 +0700

    Fix 2

--- kopasmen/kopasmen/settings.py
+++ kopasmen/kopasmen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-27nogj*$ysph%l(psf4x#$_frt)2n7fa-x#p+d*nb!yy)r#k#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
